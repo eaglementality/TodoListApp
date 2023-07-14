04329c8dfbae52919887e871caeaccef7df8e85a
eaglementality <jesseowusuyeboah99@gmail.com>
2023-07-14 14:15:31 +0000

    share data to all children

--- todolist-app/src/app/layout.js
+++ todolist-app/src/app/layout.js
@@ -1,6 +1,6 @@
 import './Styles/globals.css';
 import { Inter } from 'next/font/google'
-
+import AppContext from './Utils/context';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <AppContext>
       <body className={inter.className}>{children}</body>
+    </AppContext>  
     </html>
   )
 }
