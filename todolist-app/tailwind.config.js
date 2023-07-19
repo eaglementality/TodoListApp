58b3fb7904d0748cb3b77840abbf26c41415c1d8
eaglementality <jesseowusuyeboah99@gmail.com>
2023-07-19 17:23:31 +0000

    config files

--- todolist-app/tailwind.config.js
+++ todolist-app/tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        'text-todo': '2.rem',
+      },
+      background:{
+        'blue':'hsl(192, 100%, 67%)',
+        'violet' : 'hsl(280, 87%, 65%)'
+      },
+      backgroundColor: {
+        'Verydark':'hsl(235, 21%, 11%)',
+        'dark':'hsl(235, 24%, 19%)'
+      },
+       letterSpacing:{
+        'space':'12px'
+       },
+       screens: {
+        'mobile': '375px',
+        'desktop': '1440px',
+      },
     },
   },
   plugins: [],
