eec4421817a27046fcd08afbfdeb0ab710b4c039
Jesse <97172881+eaglementality@users.noreply.github.com>
2023-07-28 13:11:46 +0000

    updated

--- todolist-app/src/app/Backend/context.js
+++ todolist-app/src/app/Backend/context.js
@@ -1,70 +1,34 @@
 'use client'
 import { useEffect, useReducer,useContext, createContext,useState } from 'react';
 import { Reducer } from '../Backend/Reduce';
-// import { collection, getDocs, query, where } from 'firebase/firestore';
 import { collection, getDocs, addDoc} from 'firebase/firestore';
 import { db } from '../FireBase/config';
-// import { useAuth } from '../FireBase/auth';
 import  Data from '../Data/data';
 
 export const TodoContext = createContext();
 
-// const initialState = {
-//   todos: [
-//     {
-//       text: 'Eat apple',
-//       completed: false,
-//       id: 'b967afe24b23',
-//     },
-//     {
-//       text: 'Take dog for a walk',
-//       completed: true,
-//       id: '43286487fhsdjasd',
-//     },
-//     {
-//       text: 'Listen to music',
-//       completed: false,
-//       id: '54937fhajd',
-//     },
-//     {
-//       text: 'Yoga setion',
-//       completed: true,
-//       id: '43242341aaaaa',
-//     },
-//     {
-//       text: 'Prepare the dinner',
-//       completed: true,
-//       id: 'b967afe24a13',
-//     },
-//   ],
-//   visibilityFilter: 'All',
-// };
-
 const initialState = {
   todos: Data.AllTasks,
   visibilityFilter: 'All',
-  uid: '',
+  
 };
 export default function TodoContextProvider(props) {
   const [state, dispatch] = useReducer(Reducer, initialState);
   const [Darktheme, setDarktheme] = useState(true);
   const [Icon_light_and_dark, setIcon_light_and_dark]=useState(true);
   
-  // const auth = useAuth();
-//function to add to the todos in the firestore
+
 
 
   //    function to retrieve the todos from firestore
   const getTodos = async () => {
-    // const q = query(collection(db, 'users'), where('uid', '==', auth.user.uid));
     const querySnapshot = await getDocs(collection(db,'todos'));
     querySnapshot.forEach((doc) => {
-      //  doc.data() is never undefined for query doc snapshots
       dispatch({
         type: 'INITIALIZE_TODO',
         payload: {
           todos: doc.data().todos,
-          uid: doc.data().uid,
+          // uid: doc.data().uid,
         },
       });
     });
