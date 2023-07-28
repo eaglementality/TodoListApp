44396986f0c84a33e7b1e5d96059706ccc95d6dc
Jesse <97172881+eaglementality@users.noreply.github.com>
2023-07-28 13:46:06 +0000

    updated

--- todolist-app/src/app/mainSection/main_section.jsx
+++ todolist-app/src/app/mainSection/main_section.jsx
@@ -32,7 +32,7 @@ const icon_moon = <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"
 const icon_sun = <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>
 
 const [checkboxbg, setCheckboxbg] = useState(false);
-const [active,setActive]= useState({"All":true,"Active":false, "Complete":false});
+// const [active,setActive]= useState({"All":true,"Active":false, "Complete":false});
 const [isBrowser, setIsBrowser] = useState(false);
 
 
@@ -101,15 +101,15 @@ const clearcompleted=()=>{
 }
 const All_Task = () =>{ 
     dispatch({type:'SET_VISIBILITY', payload:'All'})
-    setActive({"All":true, "Active":false, "Complete":false}) 
+    // setActive({"All":true, "Active":false, "Complete":false}) 
 }
  const active_Task = ()=>{
     dispatch({type:'SET_VISIBILITY', payload:'Active'})
-    setActive({"All":false, "Active":true, "Complete": false})
+    // setActive({"All":false, "Active":true, "Complete": false})
 }
 const Completed_Task =()=>{
     dispatch({type:'SET_VISIBILITY', payload:'Completed'})
-    setActive({"All":false, "Active":false, "Complete":true})
+    // setActive({"All":false, "Active":false, "Complete":true})
 }
 
 
