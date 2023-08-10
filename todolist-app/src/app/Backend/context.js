9fdbc635144dc9c0f7c3caa97fe508aeca938cb2
eaglementality <jesseowusuyeboah99@gmail.com>
2023-08-10 04:29:27 +0000

    updated

--- todolist-app/src/app/Backend/context.js
+++ todolist-app/src/app/Backend/context.js
@@ -1,33 +1,23 @@
 'use client'
-<<<<<<< HEAD
 import{ useEffect, useReducer,useContext, createContext,useState } from 'react';
-import { reducer } from '../Backend/reduce';
-=======
 import { useEffect, useReducer,useContext, createContext,useState } from 'react';
-import { Reducer } from '../Backend/Reduce';
->>>>>>> b68bb6bd64edb96c7ca98c582095847a15814e9f
+import { Reducer } from '../Backend/reduce';
+
 import { collection, getDocs, addDoc} from 'firebase/firestore';
 import { db } from '../FireBase/config';
 import  Data from '../Data/data';
 
 export const TodoContext = createContext();
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> b68bb6bd64edb96c7ca98c582095847a15814e9f
 const initialState = {
   todos: Data.AllTasks,
   visibilityFilter: 'All',
   
 };
 export default function TodoContextProvider(props) {
-<<<<<<< HEAD
-  const [state, dispatch] = useReducer(reducer, initialState);
-=======
+  // const [state, dispatch] = useReducer(reducer, initialState);
   const [state, dispatch] = useReducer(Reducer, initialState);
->>>>>>> b68bb6bd64edb96c7ca98c582095847a15814e9f
+
   const [Darktheme, setDarktheme] = useState(true);
   const [Icon_light_and_dark, setIcon_light_and_dark]=useState(true);
   
@@ -60,10 +50,3 @@ export default function TodoContextProvider(props) {
     }>{props.children}</TodoContext.Provider>
   );
 }
-<<<<<<< HEAD
-// export function appProps(){
-//   return useContext(TodoContext);
-// }
-=======
-
->>>>>>> b68bb6bd64edb96c7ca98c582095847a15814e9f
--- todolist-app/src/app/page.js
+++ todolist-app/src/app/page.js
@@ -1,15 +1,8 @@
 'use client'
 import MainSectionlayout from "./mainSection/main_section"
-import Head from "next/head";
-<<<<<<< HEAD
-
-
-export default function Home() {
-=======
-
+// import Head from "next/head";
 export default function Home() {
 
->>>>>>> b68bb6bd64edb96c7ca98c582095847a15814e9f
     return(
         <>
         <MainSectionlayout/>
